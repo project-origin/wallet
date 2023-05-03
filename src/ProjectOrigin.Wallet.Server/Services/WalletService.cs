129984073b070da42435ce5df9127de1057374c1
Martin Schmidt <martin@scoding.dk>
2023-05-03 13:30:42 +0000

    Boilerplate commit

---
+++ src/ProjectOrigin.Wallet.Server/Services/WalletService.cs
@@ -0,0 +1,19 @@
+using Grpc.Core;
+using ProjectOrigin.Wallet.Server;
+
+namespace ProjectOrigin.Wallet.Server.Services;
+
+public class WalletService : ProjectOrigin.Wallet.V1.WalletService.WalletServiceBase
+{
+    private readonly ILogger<WalletService> _logger;
+    public WalletService(ILogger<WalletService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<V1.ReceiveResponse> ReceiveSlice(V1.ReceiveRequest request, Grpc.Core.ServerCallContext context)
+    {
+        throw new NotImplementedException("🌱");
+    }
+
+}
