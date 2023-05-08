1f450005b0f1765a7800710e19daa4c6e8ec0dc5
Martin Schmidt <martin@scoding.dk>
2023-05-08 12:27:57 +0200

    test

--- src/ProjectOrigin.Wallet.Server/Program.cs
+++ src/ProjectOrigin.Wallet.Server/Program.cs
@@ -8,12 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcReflection();
+builder.Services.AddGrpcSwagger();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "gRPC using .NET 7 Demo", Version = "v1" });
+});
 var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "gRPC using .NET7 Demo");
+}
+);
+
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<ExternalWalletService>();
+app.MapGrpcReflectionService();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
