938c9b639704d53dff8f668b8404c2d3ba993b0c
Martin Schmidt <martin@scoding.dk>
2023-05-22 08:05:09 +0000

    Cleanup after comments

--- src/ProjectOrigin.Wallet.Server/Program.cs
+++ src/ProjectOrigin.Wallet.Server/Program.cs
@@ -16,8 +16,4 @@ startup.Configure(app, builder.Environment);
 
 DatabaseUpgrader.Upgrade(app.Configuration.GetConnectionString("Database"));
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<WalletService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
