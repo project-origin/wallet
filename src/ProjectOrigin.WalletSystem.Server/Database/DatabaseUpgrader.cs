6ccc7abcf577b90daf4de087412ddf51593e36e2
Martin Schmidt <martin@scoding.dk>
2023-05-24 07:14:22 +0000

    Added server commands and migrate job

--- src/ProjectOrigin.WalletSystem.Server/Database/DatabaseUpgrader.cs
+++ src/ProjectOrigin.WalletSystem.Server/Database/DatabaseUpgrader.cs
@@ -1,4 +1,5 @@
 using DbUp;
+using DbUp.Engine;
 
 namespace ProjectOrigin.WalletSystem.Server.Database;
 
@@ -6,11 +7,7 @@ public static class DatabaseUpgrader
 {
     public static void Upgrade(string? connectionString)
     {
-        var upgradeEngine = DeployChanges.To
-            .PostgresqlDatabase(connectionString)
-            .WithScriptsEmbeddedInAssembly(typeof(DatabaseUpgrader).Assembly)
-            .LogToAutodetectedLog()
-            .Build();
+        var upgradeEngine = BuildUpgradeEngine(connectionString);
 
         var databaseUpgradeResult = upgradeEngine.PerformUpgrade();
 
@@ -18,5 +15,21 @@ public static class DatabaseUpgrader
         {
             throw databaseUpgradeResult.Error;
         }
+    }
+
+    public static bool IsUpgradeRequired(string? connectionString)
+    {
+        var upgradeEngine = BuildUpgradeEngine(connectionString);
+
+        return upgradeEngine.IsUpgradeRequired();
+    }
+
+    private static UpgradeEngine BuildUpgradeEngine(string? connectionString)
+    {
+        return DeployChanges.To
+                    .PostgresqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(typeof(DatabaseUpgrader).Assembly)
+                    .LogToAutodetectedLog()
+                    .Build();
     }
 }
--- src/ProjectOrigin.WalletSystem.Server/Program.cs
+++ src/ProjectOrigin.WalletSystem.Server/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using ProjectOrigin.WalletSystem.Server;
@@ -6,13 +8,18 @@ using ProjectOrigin.WalletSystem.Server.Database;
 var builder = WebApplication.CreateBuilder(args);
 
 var startup = new Startup(builder.Configuration);
-
 startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
-
 startup.Configure(app, builder.Environment);
 
-DatabaseUpgrader.Upgrade(app.Configuration.GetConnectionString("Database"));
+if (args.Contains("--migrate"))
+    DatabaseUpgrader.Upgrade(app.Configuration.GetConnectionString("Database"));
 
-app.Run();
+if (args.Contains("--serve"))
+{
+    if (!DatabaseUpgrader.IsUpgradeRequired(app.Configuration.GetConnectionString("Database")))
+        throw new SystemException("Database is not up to date. Please run with --migrate first.");
+
+    app.Run();
+}
