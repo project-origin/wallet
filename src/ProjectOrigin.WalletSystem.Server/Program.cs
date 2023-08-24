49096a9ee00bd47286c96c2acab59dcf80a3b6ae
tnickelsen <130448180+tnickelsen@users.noreply.github.com>
2023-08-24 15:22:54 +0200

    Added serilog (#71)

--- src/ProjectOrigin.WalletSystem.Server/Program.cs
+++ src/ProjectOrigin.WalletSystem.Server/Program.cs
@@ -2,10 +2,26 @@ using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using ProjectOrigin.WalletSystem.Server;
 using ProjectOrigin.WalletSystem.Server.Database;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+var loggerConfiguration = new LoggerConfiguration()
+    .Filter.ByExcluding("RequestPath like '/health%'")
+    .Filter.ByExcluding("RequestPath like '/metrics%'")
+    .Enrich.WithSpan();
+
+loggerConfiguration = builder.Environment.IsDevelopment()
+    ? loggerConfiguration.WriteTo.Console()
+    : loggerConfiguration.WriteTo.Console(new JsonFormatter());
+
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(loggerConfiguration.CreateLogger());
 
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
