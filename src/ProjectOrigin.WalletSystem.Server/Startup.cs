35912d24a63fbfdb71f79293561fa77176df85a7
Martin Schmidt <martin@scoding.dk>
2023-11-06 14:01:18 +0000

    port 5000 now runs http1.x, port 80 continues to run http2

--- src/ProjectOrigin.WalletSystem.Server/Startup.cs
+++ src/ProjectOrigin.WalletSystem.Server/Startup.cs
@@ -129,7 +129,6 @@ public class Startup
         {
             endpoints.MapGrpcService<WalletService>();
             endpoints.MapGrpcService<ReceiveSliceService>();
-            endpoints.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
             endpoints.MapControllers();
             endpoints.MapSwagger();
         });
