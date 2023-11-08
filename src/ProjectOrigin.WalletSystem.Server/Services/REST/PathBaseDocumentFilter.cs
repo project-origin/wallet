8047ab32fc4b4620983026d32e9aa1b4b76d4fdc
Paul Toft Duizer <paul@duizer.dk>
2023-11-08 13:43:13 +0100

    Add configurable path base (#123)
    Using values.yaml in helm one can configure the listening path base for the service.
    ```yaml
    rest.pathBase: /api
    ```

---
+++ src/ProjectOrigin.WalletSystem.Server/Services/REST/PathBaseDocumentFilter.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using ProjectOrigin.WalletSystem.Server.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace ProjectOrigin.WalletSystem.Server.Services.REST;
+
+public class PathBaseDocumentFilter : IDocumentFilter
+{
+    private readonly IOptions<RestApiOptions> _options;
+
+    public PathBaseDocumentFilter(IOptions<RestApiOptions> options)
+    {
+        _options = options;
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var paths = swaggerDoc.Paths.ToArray();
+        foreach (var path in paths)
+        {
+            swaggerDoc.Paths.Remove(path.Key);
+            var keyWithBasePath = $"{_options.Value.PathBase}{path.Key}";
+            swaggerDoc.Paths.Add(keyWithBasePath, path.Value);
+        }
+    }
+}
