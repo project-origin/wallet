e32b1f7ce99fad0257b24a3dc86ff2f5ef3768c0
Martin Schmidt <martin@scoding.dk>
2023-11-15 10:00:19 +0000

    added tests

---
+++ src/ProjectOrigin.WalletSystem.IntegrationTests/WalletControllerTests.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectOrigin.WalletSystem.IntegrationTests.TestClassFixtures;
+using ProjectOrigin.WalletSystem.IntegrationTests.TestExtensions;
+using ProjectOrigin.WalletSystem.Server.Database;
+using ProjectOrigin.WalletSystem.Server.Services.REST.v1;
+using Xunit;
+
+namespace ProjectOrigin.WalletSystem.IntegrationTests;
+
+public class WalletControllerTests : IClassFixture<PostgresDatabaseFixture>
+{
+    private readonly Fixture _fixture;
+    private readonly PostgresDatabaseFixture _dbFixture;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public WalletControllerTests(PostgresDatabaseFixture postgresDatabaseFixture)
+    {
+        _fixture = new Fixture();
+        _dbFixture = postgresDatabaseFixture;
+        _unitOfWork = _dbFixture.CreateUnitOfWork();
+    }
+
+    [Theory]
+    [InlineData("Europe/Copenhagen", new long[] { 1000, 2400, 1400 })]
+    [InlineData("Europe/London", new long[] { 1100, 2400, 1300 })]
+    [InlineData("America/Toronto", new long[] { 1600, 2400, 800 })]
+    public async Task Test_AggregateCertificates(string timezone, long[] values)
+    {
+        var issuestartDate = new DateTimeOffset(2020, 6, 1, 12, 0, 0, TimeSpan.Zero);
+        var issueEndDate = new DateTimeOffset(2020, 6, 30, 12, 0, 0, TimeSpan.Zero);
+        var queryStartDate = new DateTimeOffset(2020, 6, 8, 12, 0, 0, TimeSpan.Zero);
+        var queryEndDate = new DateTimeOffset(2020, 6, 10, 12, 0, 0, TimeSpan.Zero);
+
+        var subject = _fixture.Create<string>();
+        var controller = new WalletController
+        {
+            ControllerContext = CreateContextWithUser(subject)
+        };
+
+        var wallet = await _dbFixture.CreateWallet(subject);
+        var endpoint = await _dbFixture.CreateWalletEndpoint(wallet);
+
+        for (DateTimeOffset i = issuestartDate; i < issueEndDate; i = i.AddHours(1))
+        {
+            var cert = await _dbFixture.CreateCertificate(
+                Guid.NewGuid(),
+                _fixture.Create<string>(),
+                Server.Models.GranularCertificateType.Production,
+                start: i,
+                end: i.AddHours(1));
+            var slice = await _dbFixture.CreateSlice(endpoint, cert, new PedersenCommitment.SecretCommitmentInfo(100));
+        }
+
+        await _dbFixture.CreateCertificate(Guid.NewGuid(), _fixture.Create<string>(), Server.Models.GranularCertificateType.Production);
+
+        var result = await controller.AggregateCertificates(
+            _unitOfWork,
+            TimeAggregate.Day,
+            timezone,
+            queryStartDate.ToUnixTimeSeconds(),
+            queryEndDate.ToUnixTimeSeconds(),
+            null);
+
+        result.Value.Should().NotBeNull();
+        var resultList = result.Value!.Result;
+
+        resultList.Should().HaveCount(3);
+        resultList.Select(x => x.Quantity).Should().ContainInOrder(values);
+    }
+
+    [Theory]
+    [InlineData("Europe/Copenhagen", new long[] { 1000, 2400, 1400 })]
+    [InlineData("Europe/London", new long[] { 1100, 2400, 1300 })]
+    [InlineData("America/Toronto", new long[] { 1600, 2400, 800 })]
+    public async Task Test_AggregateClaims(string timezone, long[] values)
+    {
+        var issuestartDate = new DateTimeOffset(2020, 6, 1, 12, 0, 0, TimeSpan.Zero);
+        var issueEndDate = new DateTimeOffset(2020, 6, 30, 12, 0, 0, TimeSpan.Zero);
+        var queryStartDate = new DateTimeOffset(2020, 6, 8, 12, 0, 0, TimeSpan.Zero);
+        var queryEndDate = new DateTimeOffset(2020, 6, 10, 12, 0, 0, TimeSpan.Zero);
+
+        var subject = _fixture.Create<string>();
+        var controller = new WalletController
+        {
+            ControllerContext = CreateContextWithUser(subject)
+        };
+
+        var wallet = await _dbFixture.CreateWallet(subject);
+        var endpoint = await _dbFixture.CreateWalletEndpoint(wallet);
+
+        for (DateTimeOffset i = issuestartDate; i < issueEndDate; i = i.AddHours(1))
+        {
+            var prodCert = await _dbFixture.CreateCertificate(
+                Guid.NewGuid(),
+                _fixture.Create<string>(),
+                Server.Models.GranularCertificateType.Production,
+                start: i,
+                end: i.AddHours(1));
+            var prodSlice = await _dbFixture.CreateSlice(endpoint, prodCert, new PedersenCommitment.SecretCommitmentInfo(100));
+
+            var consCert = await _dbFixture.CreateCertificate(
+                Guid.NewGuid(),
+                _fixture.Create<string>(),
+                Server.Models.GranularCertificateType.Consumption,
+                start: i,
+                end: i.AddHours(1));
+            var consSlice = await _dbFixture.CreateSlice(endpoint, consCert, new PedersenCommitment.SecretCommitmentInfo(100));
+
+            await _dbFixture.CreateClaim(prodSlice, consSlice, Server.Models.ClaimState.Claimed);
+        }
+
+        await _dbFixture.CreateCertificate(Guid.NewGuid(), _fixture.Create<string>(), Server.Models.GranularCertificateType.Production);
+
+        var result = await controller.AggregateClaims(
+            _unitOfWork,
+            TimeAggregate.Day,
+            timezone,
+            queryStartDate.ToUnixTimeSeconds(),
+            queryEndDate.ToUnixTimeSeconds());
+
+        result.Value.Should().NotBeNull();
+        var resultList = result.Value!.Result;
+
+        resultList.Should().HaveCount(3);
+        resultList.Select(x => x.Quantity).Should().ContainInOrder(values);
+    }
+
+    private static ControllerContext CreateContextWithUser(string subject)
+    {
+        return new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext
+            {
+                User = new ClaimsPrincipal(new ClaimsIdentity(new System.Security.Claims.Claim[]
+                {
+                     new(ClaimTypes.NameIdentifier, subject),
+                })),
+            }
+        };
+
+    }
+}
