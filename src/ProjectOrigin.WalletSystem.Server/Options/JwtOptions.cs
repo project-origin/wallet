bd976f1921339ddebf17edc61e4a526d8ec1a1b6
sahma19 <saiuan@msn.com>
2024-03-04 12:33:33 +0100

    Added env variable for disabling/enabling scopes

--- src/ProjectOrigin.WalletSystem.Server/Options/JwtOptions.cs
+++ src/ProjectOrigin.WalletSystem.Server/Options/JwtOptions.cs
@@ -7,6 +7,7 @@ namespace ProjectOrigin.WalletSystem.Server.Options;
 public class JwtOptions : IValidatableObject
 {
     public bool AllowAnyJwtToken { get; init; } = false;
+    public bool EnableScopeValidation { get; set; } = false;
     public string Audience { get; init; } = string.Empty;
     public IEnumerable<JwtIssuer> Issuers { get; init; } = new List<JwtIssuer>();
 
