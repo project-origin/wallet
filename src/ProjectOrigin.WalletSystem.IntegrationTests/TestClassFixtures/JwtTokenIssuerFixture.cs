b6365a1fcf09b71c387323c2f3a34bbc010a2f78
Martin Schmidt <martin@scoding.dk>
2024-03-05 08:25:40 +0000

    Refactor JwtTokenIssuerFixture class

--- src/ProjectOrigin.WalletSystem.IntegrationTests/TestClassFixtures/JwtTokenIssuerFixture.cs
+++ src/ProjectOrigin.WalletSystem.IntegrationTests/TestClassFixtures/JwtTokenIssuerFixture.cs
@@ -13,14 +13,15 @@ namespace ProjectOrigin.WalletSystem.IntegrationTests.TestClassFixtures;
 public sealed class JwtTokenIssuerFixture : IDisposable
 {
     private readonly RSA _rsa = RSA.Create();
+
     public string KeyType => "RSA";
-    public string Issuer { get; init; } = "TestIssuer";
-    public string Audience => "WalletSystem";
     public string Algorithm => SecurityAlgorithms.RsaSha256;
     public byte[] PublicKeyInfo => _rsa.ExportSubjectPublicKeyInfo();
     public int ExpirationMinutes => 60;
+    public string PemFilepath { get; private init; }
+    public string Audience => "WalletSystem";
 
-    public string PemFilepath { get; }
+    public string Issuer { get; init; } = "TestIssuer";
 
     public JwtTokenIssuerFixture()
     {
@@ -69,6 +70,15 @@ public sealed class JwtTokenIssuerFixture : IDisposable
         return (subject, headers);
     }
 
+    public string GetJsonOpenIdConfiguration()
+    {
+        return JsonSerializer.Serialize(new
+        {
+            issuer = $"{Issuer}",
+            jwks_uri = $"{Issuer}/keys",
+        });
+    }
+
     public string GetJsonKeys()
     {
         return JsonSerializer.Serialize(new
@@ -83,62 +93,6 @@ public sealed class JwtTokenIssuerFixture : IDisposable
                         e = Convert.ToBase64String(_rsa.ExportParameters(true).Exponent!),
                     }
             }
-        });
-    }
-
-    public string GetJsonOpenIdConfiguration()
-    {
-        return JsonSerializer.Serialize(new
-        {
-            issuer = $"{Issuer}",
-            authorization_endpoint = $"{Issuer}/auth",
-            token_endpoint = $"{Issuer}/token",
-            jwks_uri = $"{Issuer}/keys",
-            userinfo_endpoint = $"{Issuer}/userinfo",
-            device_authorization_endpoint = $"{Issuer}/device/code",
-            grant_types_supported = new[]{
-                "authorization_code",
-                "refresh_token",
-                "urn:ietf:params:oauth:grant-type:device_code",
-                "urn:ietf:params:oauth:grant-type:token-exchange"
-            },
-            response_types_supported = new[]{
-                "code"
-            },
-            subject_types_supported = new[]{
-                "public"
-            },
-            id_token_signing_alg_values_supported = new[]{
-                Algorithm
-            },
-            code_challenge_methods_supported = new[]{
-                "S256",
-                "plain"
-            },
-            scopes_supported = new[]{
-                "openid",
-                "email",
-                "groups",
-                "profile",
-                "offline_access"
-            },
-            token_endpoint_auth_methods_supported = new[]{
-                "client_secret_basic",
-                "client_secret_post"
-            },
-            claims_supported = new[]{
-                "iss",
-                "sub",
-                "aud",
-                "iat",
-                "exp",
-                "email",
-                "email_verified",
-                "locale",
-                "name",
-                "preferred_username",
-                "at_hash"
-            },
         });
     }
 
