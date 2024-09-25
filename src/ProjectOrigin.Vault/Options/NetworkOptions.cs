1475b8be24ae560b4a9bf3a01f2ec56901e80562
Martin Schmidt <martin@scoding.dk>
2024-09-25 06:59:28 +0000

    Added chronicler proto and options

--- src/ProjectOrigin.Vault/Options/NetworkOptions.cs
+++ src/ProjectOrigin.Vault/Options/NetworkOptions.cs
@@ -16,9 +16,17 @@ public record RegistryInfo
 public class AreaInfo
 {
     public required IList<KeyInfo> IssuerKeys { get; set; }
+    public ChroniclerInfo? Chronicler { get; init; }
 }
 
 public record KeyInfo
 {
     public required string PublicKey { get; init; }
+}
+
+public record ChroniclerInfo
+{
+    public required string Url { get; init; }
+
+    public required IList<KeyInfo> SignerKeys { get; set; } = new List<KeyInfo>();
 }
