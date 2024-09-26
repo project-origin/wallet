955abc09f51e8de2cb29ad1716a4f7041be52a50
tnickelsen <TNE@energinet.dk>
2024-09-26 10:37:06 +0200

    added network options

--- src/ProjectOrigin.Vault/CommandHandlers/CheckForWithdrawnCertificatesCommandHandler.cs
+++ src/ProjectOrigin.Vault/CommandHandlers/CheckForWithdrawnCertificatesCommandHandler.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Threading.Tasks;
 using MassTransit;
+using Microsoft.Extensions.Options;
 using ProjectOrigin.Vault.Database;
 using ProjectOrigin.Vault.Models;
+using ProjectOrigin.Vault.Options;
 
 namespace ProjectOrigin.Vault.CommandHandlers;
 
@@ -13,13 +15,17 @@ public record CheckForWithdrawnCertificatesCommand
 public class CheckForWithdrawnCertificatesCommandHandler : IConsumer<CheckForWithdrawnCertificatesCommand>
 {
     private readonly IUnitOfWork _unitOfWork;
-    public CheckForWithdrawnCertificatesCommandHandler(IUnitOfWork unitOfWork)
+    private readonly NetworkOptions _networkOptions;
+    public CheckForWithdrawnCertificatesCommandHandler(IUnitOfWork unitOfWork, IOptions<NetworkOptions> networkOptions)
     {
         _unitOfWork = unitOfWork;
+        _networkOptions = networkOptions.Value;
     }
 
     public async Task Consume(ConsumeContext<CheckForWithdrawnCertificatesCommand> context)
     {
+        var stamps = _networkOptions.Stamps;
+
         var withdrawnCursor = new WithdrawnCursor { StampName = "Narnia", SyncPosition = 1, LastSyncDate = DateTimeOffset.UtcNow };
         await _unitOfWork.WithdrawnCursorRepository.InsertWithdrawnCursor(withdrawnCursor);
         _unitOfWork.Commit();
--- src/ProjectOrigin.Vault/Options/NetworkOptions.cs
+++ src/ProjectOrigin.Vault/Options/NetworkOptions.cs
@@ -6,6 +6,7 @@ public record NetworkOptions
 {
     public IDictionary<string, RegistryInfo> Registries { get; init; } = new Dictionary<string, RegistryInfo>();
     public IDictionary<string, AreaInfo> Areas { get; init; } = new Dictionary<string, AreaInfo>();
+    public IDictionary<string, StampInfo> Stamps { get; init; } = new Dictionary<string, StampInfo>();
 }
 
 public record RegistryInfo
@@ -21,4 +22,9 @@ public class AreaInfo
 public record KeyInfo
 {
     public required string PublicKey { get; init; }
+}
+
+public record StampInfo
+{
+    public required string Url { get; init; }
 }
