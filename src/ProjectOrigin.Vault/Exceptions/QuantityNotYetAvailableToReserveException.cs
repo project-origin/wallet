500dc0892bcf563dfd6d70f61a9b17416f7d03b6
Theis Nickelsen <130448180+tnickelsen@users.noreply.github.com>
2024-12-19 08:40:31 +0100

    QuantityNotYetAvailableToReserveException is now a normal exception (#492)

--- src/ProjectOrigin.Vault/Exceptions/QuantityNotYetAvailableToReserveException.cs
+++ src/ProjectOrigin.Vault/Exceptions/QuantityNotYetAvailableToReserveException.cs
@@ -1,10 +1,9 @@
 using System;
-using System.ComponentModel;
 
 namespace ProjectOrigin.Vault.Exceptions;
 
 [Serializable]
-public class QuantityNotYetAvailableToReserveException : WarningException
+public class QuantityNotYetAvailableToReserveException : Exception
 {
     public QuantityNotYetAvailableToReserveException(string? message) : base(message)
     {
