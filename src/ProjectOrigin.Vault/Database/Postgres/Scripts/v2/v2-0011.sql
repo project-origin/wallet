d5132426e6d6a3ee9483bc53a05fccf98c76a379
tnickelsen <TNE@energinet.dk>
2025-01-10 12:02:46 +0100

    added idx

--- src/ProjectOrigin.Vault/Database/Postgres/Scripts/v2/v2-0011.sql
+++ src/ProjectOrigin.Vault/Database/Postgres/Scripts/v2/v2-0011.sql
@@ -9,3 +9,7 @@ CREATE INDEX IF NOT EXISTS idx_attributes_certificate_id_registry
 CREATE INDEX IF NOT EXISTS idx_wallet_attributes_certificate_wallet_attribute
     ON public.wallet_attributes
     (certificate_id, wallet_id, registry_name);
+
+CREATE INDEX IF NOT EXISTS idx_wallet_slices_wallet_endpoint_id
+    ON public.wallet_slices
+    (wallet_endpoint_id);
