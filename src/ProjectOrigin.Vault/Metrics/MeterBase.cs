b81ed03dbfed01f83b7207e5d5c33159b78366fa
sahma19 <saiuan@msn.com>
2025-01-13 13:00:31 +0100

    no longer depend on iconfiguration

--- src/ProjectOrigin.Vault/Metrics/MeterBase.cs
+++ src/ProjectOrigin.Vault/Metrics/MeterBase.cs
@@ -1,10 +1,9 @@
 using System.Diagnostics.Metrics;
-using Microsoft.Extensions.Configuration;
 
 namespace ProjectOrigin.Vault.Metrics;
 
-public class MeterBase(IMeterFactory meterFactory, IConfiguration configuration)
+public class MeterBase(IMeterFactory meterFactory)
 {
     public const string MeterName = "ProjectOrigin.Vault";
-    public Meter Meter { get; } = meterFactory.Create(configuration["VaultMeterName"] ?? MeterName);
+    public Meter Meter { get; } = meterFactory.Create(MeterName);
 }
