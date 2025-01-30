9ed1939db8360190551e095bd443007849e60014
sahma19 <saiuan@msn.com>
2025-01-30 13:24:21 +0100

    remove todo

--- src/ProjectOrigin.Vault/CommandHandlers/ClaimCertificateCommandHandler.cs
+++ src/ProjectOrigin.Vault/CommandHandlers/ClaimCertificateCommandHandler.cs
@@ -67,7 +67,7 @@ public class ClaimCertificateCommandHandler : IConsumer<ClaimCertificateCommand>
         catch (QuantityNotYetAvailableToReserveException ex)
         {
             _unitOfWork.Rollback();
-            // TODO: Add jitter to delay the retry
+            // Add jitter to delay the retry
             await Task.Delay(TimeSpan.FromSeconds(Random.Shared.Next(5, 10)));
             _logger.LogWarning(ex, "Failed to handle claim at this time.");
             throw;
