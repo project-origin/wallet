2cfb643998adee3782e6fc25dab169d29fc6bd22
Christoffer Frydkjær <christofferfk@gmail.com>
2025-03-10 14:02:59 +0100

    change format sql

--- src/ProjectOrigin.Vault/Database/Postgres/Scripts/v2/v2-0013.sql
+++ src/ProjectOrigin.Vault/Database/Postgres/Scripts/v2/v2-0013.sql
@@ -1,16 +1,7 @@
 CREATE TABLE IF NOT EXISTS OutboxMessages
 (
-    id
-    uuid
-    NOT
-    NULL
-    PRIMARY
-    KEY,
-    message_type
-    VARCHAR
-(
-    250
-) NOT NULL,
+    id uuid NOT NULL PRIMARY KEY,
+    message_type VARCHAR(250) NOT NULL,
     json_payload TEXT NOT NULL,
     created timestamp with time zone NOT NULL
-                          );
+);
