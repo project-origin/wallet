52ab806d527d4acc99847f3c76cc9274d5b108cb
Christoffer Frydkjær <christofferfk@gmail.com>
2025-06-12 13:55:16 +0200

    format

--- src/ProjectOrigin.Vault/Repositories/CertificateRepository.cs
+++ src/ProjectOrigin.Vault/Repositories/CertificateRepository.cs
@@ -338,16 +338,16 @@ public class CertificateRepository : ICertificateRepository
             ";
 
         using (var gridReader = await _connection.QueryMultipleAsync(sql, new
-               {
-                   filter.Owner,
-                   filter.Start,
-                   filter.End,
-                   filter.Type,
-                   filter.Skip,
-                   filter.Limit,
-                   timeAggregate = filter.TimeAggregate.ToString().ToLower(),
-                   filter.TimeZone
-               }))
+        {
+            filter.Owner,
+            filter.Start,
+            filter.End,
+            filter.Type,
+            filter.Skip,
+            filter.Limit,
+            timeAggregate = filter.TimeAggregate.ToString().ToLower(),
+            filter.TimeZone
+        }))
         {
             var totalCount = await gridReader.ReadSingleAsync<int>();
             var certificates = await gridReader.ReadAsync<AggregatedCertificatesViewModel>();
